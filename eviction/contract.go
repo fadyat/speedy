ce8805c4584234a18f1b224769129b6389d39789
fadyat <fadyat@icloud.com>
2023-10-13 23:29:27 +0300

    feat: provide eviction algorithm interface #2

---
+++ eviction/contract.go
@@ -0,0 +1,30 @@
+package eviction
+
+import "errors"
+
+var (
+	noKey              string
+	ErrInvalidCapacity = errors.New("algorithm: capacity is too small")
+)
+
+type Node struct {
+	key  string
+	val  string
+	prev *Node
+	next *Node
+}
+
+type Algorithm interface {
+
+	// Get returns the value associated with the given key.
+	// - If the key exists, it returns the value and true, otherwise it returns
+	//   nil and false.
+	Get(key string) (string, bool)
+
+	// Put inserts the given key-value pair into the cache.
+	// - If the key already exists, it updates the value.
+	// - If the key does not exist, it inserts the key-value pair.
+	// - If the cache is full, it evicts the least recently used item before
+	//   inserting the new key-value pair.
+	Put(key string, val string)
+}
